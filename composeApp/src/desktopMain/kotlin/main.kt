6e4f291412f69f0f7d2fadc2185ace67bc62961c
Paul Ruvolo <paullundyruvolo@gmail.com>
2024-03-10 20:38:57 -0400

    Changed window name

--- composeApp/src/desktopMain/kotlin/main.kt
+++ composeApp/src/desktopMain/kotlin/main.kt
@@ -3,7 +3,7 @@ import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
 fun main() = application {
-    Window(onCloseRequest = ::exitApplication, title = "ComposeDemo") {
+    Window(onCloseRequest = ::exitApplication, title = "Min Heap Demonstration") {
         App()
     }
 }
